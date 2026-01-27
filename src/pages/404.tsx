642195d86689849f2e8f5c5cc66ffc45875e3485
rsl150101 <dnwnd501@naver.com>
2026-01-27 20:43:08 +0900

    style: 404 페이지 디자인 커스텀 및 한글화
    - 기본 인라인 스타일을 제거하고 styled-components 적용
    - 글로벌 테마(text, primary color 등)를 적용하여 디자인 통일
    - 에러 문구를 한글로 변경 및 홈으로 이동하는 아이콘 추가

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,75 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import { Link, HeadFC, PageProps } from "gatsby";
+import styled from "styled-components";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.subText};
+  font-size: ${({ theme }) => theme.fontSize.ml};
+  padding: 50px 0;
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+  h1 {
+    color: ${({ theme }) => theme.colors.text};
+    font-size: ${({ theme }) => theme.fontSize.xl};
+    margin: 50px 0;
+    font-weight: 800;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.fontSize.lg};
+    margin-bottom: 10px;
+    font-weight: 600;
+  }
+
+  h4 {
+    margin-bottom: 30px;
+    font-size: ${({ theme }) => theme.fontSize.md};
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: underline 1px ${({ theme }) => theme.colors.primary};
+    text-underline-offset: 3px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.primary};
+    path {
+      stroke: ${({ theme }) => theme.colors.primary};
+    }
+  }
+`;
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Container>
+      <h1>404 Not found</h1>
+      <h3>죄송합니다. 페이지를 찾을 수 없습니다.</h3>
+      <h4>주소가 잘못 입력 되었거나, 변경 혹은 삭제된 페이지입니다.</h4>
+      <Link to="/">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="size-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        홈으로
+      </Link>
+    </Container>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Not found</title>;
